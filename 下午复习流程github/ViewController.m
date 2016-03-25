6ff322878dea08d900b9bafa597465ea4d1e7a7d
ChangLetian <CLT@ChangLetiandeMacBook-Pro.local>
2016-03-25 14:26:03 +0800

    jia

--- 下午复习流程github/ViewController.m
+++ 下午复习流程github/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    label.backgroundColor = [UIColor redColor];
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
